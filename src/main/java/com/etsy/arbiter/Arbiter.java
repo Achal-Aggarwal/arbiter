c6e49bac5ac72878f263833fbcad996d68411227
Achal-Aggarwal <theachalaggarwal@gmail.com>
2016-04-23 22:17:15 -0500

    Adding modification copyright on modified files.

--- src/main/java/com/etsy/arbiter/Arbiter.java
+++ src/main/java/com/etsy/arbiter/Arbiter.java
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * This file has been modified from its original licensed form.
+ * Modifications are Copyright (C) 2016 Achal Aggarwal (achalaggarwal.net).
  */
 
 package com.etsy.arbiter;
